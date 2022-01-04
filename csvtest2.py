d5030504f6147b7f2a6145404187979606c639cb
jphpss <97118114+jphpss@users.noreply.github.com>
2022-01-04 14:17:17 -0300

    Add files via upload

---
+++ csvtest2.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# The above line specifies path to your python executable.
+
+import os
+import csv
+import sys
+
+# path to source file
+PATH = 'output.csv'
+# case-insensitive string to search for
+STRING = 'German'
+# list of fieldnames to output (empty [] for all fields)
+FIELDS = ['Company name', 'Lifecycle Stage', 'Last Contacted', 'Company owner', 'LinkedIn Bio']
+# output file
+OUTFILE = 'output2.csv'
+
+i = 0 # counter
+
+assert os.path.isfile(PATH)
+
+with open(PATH, newline='') as f:
+    reader = csv.DictReader(f)
+    with open(OUTFILE, 'w', newline='') as g:
+
+        if FIELDS and set(FIELDS).issubset(set(reader.fieldnames)):
+            # subset of fields
+            writer = csv.DictWriter(g, FIELDS, extrasaction='ignore')
+        elif not FIELDS:
+            # all fields
+            writer = csv.DictWriter(g, reader.fieldnames)
+        else:
+            # incorrect fieldname
+            print('FIELDS contains an incorrect field name')
+            sys.exit(1)
+
+        writer.writeheader()
+        for d in reader:
+            if STRING.lower() in ','.join(list(d.values())).lower():
+                writer.writerow(d)
+                i += 1
+
+        print(f'{i} records matched and written')
+
